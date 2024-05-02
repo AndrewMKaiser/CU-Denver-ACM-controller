6e4116b3b6d50c8d078f53a08cd76f9ae0f28e6e
AndrewMKaiser <andrew.m.kaiser77@gmail.com>
2024-05-01 23:15:21 -0600

    add specifications for all blog fields

--- models/Blog.js
+++ models/Blog.js
@@ -5,10 +5,24 @@ require('dotenv').config();
 mongoose.set('useCreateIndex', true);
 
 var BlogSchema = new Schema({
-    title: String,
-    body: String,
-    date: Date,
-    author: String
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Blog', BlogSchema);
