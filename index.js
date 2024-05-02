18b5f7738aaac92c4a98c6cf1ef1d98d1367284c
Andrew Kaiser <144293437+AndrewMKaiser@users.noreply.github.com>
2024-05-01 21:08:04 -0600

    add db connection string

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
+require('dotenv').config();
+
+mongoose.connect(process.env.DB,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 
 var app = express();
