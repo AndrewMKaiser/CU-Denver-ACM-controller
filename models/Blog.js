07930b94e9f91adb7abecef6a3950512e44d2558
AndrewMKaiser <andrew.m.kaiser77@gmail.com>
2024-05-02 00:59:35 -0600

    add pre-remove middleware for deleting all replies for a deleted blog post

--- models/Blog.js
+++ models/Blog.js
@@ -25,4 +25,9 @@ var BlogSchema = new Schema({
     }
 });
 
+// Middleware to delete all replies associated with a blog post when the blog post is deleted
+BlogSchema.pre('remove', function(next) {
+    this.model('Reply').deleteMany({ blogId: this._id }, next);
+});
+
 module.exports = mongoose.model('Blog', BlogSchema);
