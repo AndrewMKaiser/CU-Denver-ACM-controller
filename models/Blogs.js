a8d50a4b2caaac4e06e455d0c2d40a92dedaab90
Andrew Kaiser <144293437+AndrewMKaiser@users.noreply.github.com>
2024-05-01 21:07:56 -0600

    move to models dir

--- models/Blogs.js
+++ models/Blogs.js
@@ -1,14 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-console.log(process.env.DB);
-mongoose.connect(process.env.DB, { useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 
-// User schema
 var BlogSchema = new Schema({
     title: String,
     body: String,
@@ -16,5 +12,4 @@ var BlogSchema = new Schema({
     author: String
 });
 
-// Return the model of our server
 module.exports = mongoose.model('Blog', BlogSchema);
