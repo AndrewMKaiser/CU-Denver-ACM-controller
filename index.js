1978113b6e003efbfc6a00c4a56be856dc3f54f7
AndrewMKaiser <andrew.m.kaiser77@gmail.com>
2024-05-02 01:22:35 -0600

    implement content-type checking, rate limiting and general error-checking middleware into the server

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');
 const cors = require('cors');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const rateLimit = require("express-rate-limit");
 const blogRouter = require('./routes/blogRouter');
 const signupRouter = require('./routes/signupRouter');
 const signinRouter = require('./routes/signinRouter');
@@ -20,6 +21,22 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Middleware to check for content-type
+app.use((req, res, next) => {
+    if (req.headers['content-type'] !== 'application/json') {
+        res.status(415).send('Server only accepts application/json data.');
+    } else {
+        next();
+    }
+});
+
+// Middleware to check for rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
 var router = express.Router();
 app.use('/', router);
 app.use('/blogs', blogRouter);
@@ -27,4 +44,9 @@ app.use('/signup', signupRouter);
 app.use('/signin', signinRouter);
 
 app.listen(process.env.PORT || 3000);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Error occurred in application.');
+});
+
 module.exports = app; // for testing
