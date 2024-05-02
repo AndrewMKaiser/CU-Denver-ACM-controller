978f237bb4fd15d8958df28897b847d13a728dd9
AndrewMKaiser <andrew.m.kaiser77@gmail.com>
2024-05-02 00:47:06 -0600

    Update index.js to include routers

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // TODO: Add content-type checks, rate limiting, logging and monitoring, implement RS256 for JWT auth
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var cors = require('cors');
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+const cors = require('cors');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const blogRouter = require('./routes/blogRouter');
+const signupRouter = require('./routes/signupRouter');
+const signinRouter = require('./routes/signinRouter');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB,
@@ -11,5 +16,15 @@ mongoose.connect(process.env.DB,
 );
 
 var app = express();
-var router = express.Router();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+var router = express.Router();
+app.use('/', router);
+app.use('/blogs', blogRouter);
+app.use('/signup', signupRouter);
+app.use('/signin', signinRouter);
+
+app.listen(process.env.PORT || 3000);
+module.exports = app; // for testing
