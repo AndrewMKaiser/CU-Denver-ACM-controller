bc1e9f78c26032d4efcfdc2c3437de3f79f8fa4e
AndrewMKaiser <andrew.m.kaiser77@gmail.com>
2024-05-02 23:20:19 -0600

    comment out unnecessary middleware

--- index.js
+++ index.js
@@ -20,22 +20,25 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
 
-// Middleware to check for content-type
-app.use((req, res, next) => {
-    if (req.headers['content-type'] !== 'application/json') {
-        res.status(415).send('Server only accepts application/json data.');
-    } else {
-        next();
-    }
-});
+var router = express.Router();
 
-// Middleware to check for rate limiting
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
+// // Middleware to check for content-type
+// app.use((req, res, next) => {
+//     if (req.headers['content-type'] !== 'application/json') {
+//         res.status(415).send('Server only accepts application/json data.');
+//     } else {
+//         next();
+//     }
+// });
+
+// // Middleware to check for rate limiting
+// const limiter = rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100 // limit each IP to 100 requests per windowMs
+// });
+// app.use(limiter);
 
 var router = express.Router();
 app.use('/', router);
