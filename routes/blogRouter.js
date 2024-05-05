94fa0c302b27f352b060b82022c83ea64c34b999
Bryce <brycecbak@gmail.com>
2024-05-04 22:10:01 -0600

    didn't work. Try again

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -59,14 +59,10 @@ router.put('/:blogId', isAuthenticated, async (req, res) => {
 // DELETE to delete a blog post
 router.delete('/:blogId', isAuthenticated, async (req, res) => {
     try {
-        const blogId = mongoose.Types.ObjectId(req.params.blogId);
-        //var userId = mongoose.Types.ObjectId(req.query.userId);
-        //const blog = await Blog.findOne({ _id: blogId, author: userId });
-        //const blog = await Blog.findOne({ _id: blogId });
-        //if (!blog) {
-        //    return res.status(404).json({ success: false, message: 'Blog not found or you are unauthorized to delete this blog.' });
-        //}
-        const blog = await Blog.findOneAndDelete( { _id: blogId } );
+        const blog = await Blog.findOneAndDelete({ _id: req.params.blogId });
+        if (!blog) {
+            return res.status(404).json({ success: false, message: 'Blog not found or you are unauthorized to update this blog.' });
+        }
         res.json({ success: true, message: 'Blog and all associated replies deleted.', blog: blog });
     } catch (err) {
         res.status(500).json({ success: false, message: 'An error occurred while deleting the blog.', error: err });
